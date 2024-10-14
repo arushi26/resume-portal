4c01cd683a513803c05e6752ad12450712d4b349
Arushi <arushipant@ymail.com>
2024-10-14 15:23:44 -0700

    1. Spring Boot application created; 2. Added User and repository JPA infrastructure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # resume-portal
+
+Steps to run the code:
+
+1. Clone the repo
+
+2. Install MySQL
+
+3. Update application.properties file to use the MySQL connection locally. Specifically:
+
+spring.datasource.url, spring.datasource.username and spring.datasource.password
+
+4. Run the main application:
+
+ResumePortalApplication
---
+++ src/main/java/com/arushi/resume_portal/HomeController.java
@@ -0,0 +1,13 @@
+package com.arushi.resume_portal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<html>Hello! <br/> <a href='/logout'>Logout</a></html>";
+    }
+}

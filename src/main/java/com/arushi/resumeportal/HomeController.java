b07f8f395cdf5fc367092acec69ecb8355d239e9
Arushi <arushipant@ymail.com>
2024-10-23 19:52:55 -0700

    Added a profile view path + Thymeleaf

--- src/main/java/com/arushi/resumeportal/HomeController.java
+++ src/main/java/com/arushi/resumeportal/HomeController.java
@@ -1,21 +1,28 @@
 package com.arushi.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "<html>Hello! <br/> <a href='/logout'>Logout</a></html>";
-    }
+//    @GetMapping("/")
+//    public String home() {
+//        return "<html>Hello! <br/> <a href='/logout'>Logout</a></html>";
+//    }
+//
+//    @GetMapping("/edit")
+//    public String edit() {
+//        return "edit page";
+//    }
 
-    @GetMapping("/edit")
-    public String edit() {
-        return "edit page";
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable("id") String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "profile";
     }
-
 
 
 }
